b6ceffcc54964ba7ad05aa9b63a55fc497c505d2
Miroslav Tzonkov <miro@smooker.org>
2024-02-29 14:31:20 +0200

    new file:   gnuplot.txt

--- test.pl
+++ test.pl
@@ -19,11 +19,18 @@ binmode $FILEOUT;
 my $data;
 my $data_1to1;
 
+my @arrX;
+my @arrY;
+
 my $cnt = 0;
 my $cnt2 = 1;
 my $bukva = "X";
+my $nomer = 1;
 
-printf("addr\t1to1\tfile\tdiffbits\tdiffmm\n");
+my $y = 0;
+my $x = 0;
+
+printf("addr\tnum\t1to1\tfile\tdiffbits\tdiffmm\n");
 
 while ((my $n = read $FILE, $data, 2) != 0) {
 	my $n1 = read $FILE1TO1, $data_1to1, 2;
@@ -32,17 +39,59 @@ while ((my $n = read $FILE, $data, 2) != 0) {
 	#my $uint16 = pack 'S', $data;
 	my $diff = $hex_1to1-$hex;
 	my $diffinmm = $diff/358;
-	printf("%04x:\t%d\t%d\t%d\t%f\n", $cnt, $hex_1to1, $hex, $diff, $diffinmm);
-	print $FILEOUT pack 's*', $hex-1;
+	printf("%04x:\t%02d\t%d\t%d\t%d\t%f\n", $cnt, $nomer, $hex_1to1, $hex, $diff, $diffinmm);
+	print $FILEOUT pack 's*', $hex; #the same as original
+
+	if ($cnt < 8450) {
+		$arrX[$y][$x] = $hex_1to1;
+		if ($x == 64) {	
+			$y++;
+			$x=0;
+		} else {
+			$x++;
+		}
+	} else {
+		$arrY[$y][$x] = $hex_1to1;
+		if ($y == 64) {
+			$x++;
+			$y=0;
+		} else {
+			$y++;
+		}
+	}
+
 	$cnt+=2;
+	$nomer++;
 	if ( ($cnt % 130) == 0 ) {
 		print"== $bukva ========== ".$cnt2++."\n";
+		$nomer = 1;
 	}
 	if ( ($cnt % 8450) == 0 ) {
                 #print"## Y ########## ".$cnt2++."\n";
 		$bukva = "Y";
+		$cnt2 = 1;
+		$y=0;
         }
 }
+open my $F2, '>vgz.dat' or die;
+
+	foreach my $row (@arrX) {
+		#print "X\t";
+		my $y = 0;
+		for (my $i=0;$i<65;$i++) {
+			print $F2 $row->[$i]."\t".$arrY[$y][$i]."\n";
+		}
+		#print "\n";
+	}
+close($F2);
+#	foreach my $row (@arrY) {
+#		print "Y\t";
+#		for (my $i=0;$i<65;$i++) {
+#			print $row->[$i]."\t";
+#		}
+#		print "\n";
+#	}
+
 
 close($FILE1TO1);
 close($FILEOUT);
